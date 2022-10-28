638feeb59aa73d52e07398a30808ba6faaff8f6b
Chanuzu <114120106+Chanuzu@users.noreply.github.com>
2022-10-28 15:07:02 +0800

    Add files via upload

--- admin/instructor.php
+++ admin/instructor.php
@@ -1,14 +1,17 @@
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/scripts.php'; ?>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+
+
 
 
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
-
   <?php include 'includes/navbar.php'; ?>
   <?php include 'includes/menubar.php'; ?>
-
+ 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -51,13 +54,14 @@
                <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
             </div>
             <div class="box-body">
-              <table id="example1" class="table table-bordered">
-                <thead>
-                  <th>Instructors ID</th>
-                  <th>Name</th>
-                  <th>Department</th>
-                  <th>Rate/hour</th>
-                  <th>Tools</th>
+              <table id="myTable" class="table table-bordered">
+                <thead style="color: white; background-color: #1e272c">
+                  <th style="text-align: center">Instructors ID</th>
+                  <th style="text-align: center">Name</th>
+                  <th style="text-align: center">Department</th>
+                  <th style="text-align: center">Rate/hour</th>
+                  <th style="text-align: center">Load Sheet</th>
+                  <th style="text-align: center; background-color: white; color: black">Tools  </th>
                 </thead>
 
                 <?php
@@ -66,17 +70,20 @@
                     while($row = $query->fetch_assoc()){
                 ?>
                         <tr>
-                          <td><?php echo $row['instructors_id']; ?></td>
-                          <td><?php echo $row['firstname'].' '.$row['lastname']; ?></td>
-                          <td><?php echo $row['department']; ?></td>
-                          <td><?php echo $row['rate']; ?></td>
-                          <td>
-                          <a href="load_sheet.php?id=<?php echo $row['instructors_id']; ?>"><button class="btn btn-info btn-sm view btn-flat" data-id="<?php echo $row['insid']; ?>"><i class="fa fa-file"></i> Load Sheet</button></a>
-                            <button class="btn btn-success btn-sm edit btn-flat" data-id="<?php echo $row['insid']; ?>"><i class="fa fa-edit"></i> Edit</button>
-                            <button class="btn btn-danger btn-sm delete btn-flat" data-id="<?php echo $row['insid']; ?>"><i class="fa fa-trash"></i> Delete</button>
+                          <td style="border: 1px solid black"><?php echo $row['instructors_id']; ?></td>
+                          <td style="border: 1px solid black"><?php echo $row['firstname'].' '.$row['lastname']; ?></td>
+                          <td style="border: 1px solid black"><?php echo $row['department']; ?></td>
+                          <td style="border: 1px solid black"><?php echo $row['rate']; ?></td>
+                          <td style="border: 1px solid black">
+                          <a href="load_sheet.php?id=<?php echo $row['instructors_id']; ?>"><button class="btn btn-info btn-sm view btn-flat" data-id="<?php echo $row['insid']; ?>"><i class="fa fa-file"></i>   View Load Sheet</button></a>
+                          </td>  
+                         <td style="text-align: center"> 
+                        <button type="button" class="btn btn-sm btn-success mt-2" data-toggle="modal" data-target="#edit-employee-modal"  data-id="<?php echo $row['instructors_id']; ?>"> Edit </button>                        
+                        <button type="button" value="<?=$row['id'];?>" class="deleteStudentBtn btn btn-danger btn-sm"><span class="fa fa-trash"></span>  Delete</button>
                           </td>
                         </tr>
               <?php
+             
               }
               ?>
                 </tbody>
@@ -90,10 +97,54 @@
                 
     
   
-<?php include 'includes/instructor_modal.php'; ?>
-</div>
 
+</div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/scripts.js"></script>	
+   
+    
+<script>
+        $(document).on('click', '.deleteStudentBtn', function (e) {
+            e.preventDefault();
+
+            if(confirm('Are you sure you want to delete this data?'))
+            {
+                var instructors_id = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: "code.php",
+                    data: {
+                        'delete_student': true,
+                        'instructors_id': instructors_id
+                    },
+                    success: function (response) {
+
+                        var res = jQuery.parseJSON(response);
+                        if(res.status == 500) {
+
+                            alert(res.message);
+                        }else{
+                            alertify.set('notifier','position', 'top-right');
+                            alertify.success(res.message);
+
+                            $('#myTable').load(location.href + " #myTable");
+                        }
+                    }
+                });
+            }
+        });
+        </script>
+       
+       
+     
+
+ 
 <?php include 'includes/datatable_initializer.php'; ?>
+<?php include 'includes/instructor_addmodal.php'; ?>
+
 
 </body>
 </html>
---
+++ admin/instructor_add.php
@@ -0,0 +1,37 @@
+<?php
+	include 'includes/session.php';
+
+	if(isset($_POST['add'])){
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$user = $_POST['username'];
+		$pass = $_POST['password'];
+		$department = $_POST['department'];
+		$rate = $_POST['rate'];
+		
+		//creating instructorid
+		$letters = '';
+		$numbers = '';
+		foreach (range('A', 'Z') as $char) {
+		    $letters .= $char;
+		}
+		for($i = 0; $i < 10; $i++){
+			$numbers .= $i;
+		}
+		$instructor_id = substr(str_shuffle($letters), 0, 3).substr(str_shuffle($numbers), 0, 9);
+		//
+		$sql = "INSERT INTO instructors (instructors_id, firstname, lastname, username, password, department, rate) VALUES ('$instructor_id', '$firstname', '$lastname',  '$user', '$pass', '$department', '$rate')";
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Instructor added successfully';
+		}
+		else{
+			$_SESSION['error'] = $conn->error;
+		}
+
+	}
+	else{   
+		$_SESSION['error'] = 'Fill up add form first';
+	}
+
+	header('location: instructor.php');
+?>
